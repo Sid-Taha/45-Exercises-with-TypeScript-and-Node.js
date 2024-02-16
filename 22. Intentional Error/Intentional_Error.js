c55cf14db82dd29de6d853c34efcf09fc0a32469
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 21:02:43 +0500

    Add files via upload

---
+++ 22. Intentional Error/Intentional_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+//Written by: Taha Ahmed
+//DATE: 16-2-24
+// Creating array
+let myArray = [1, 2, 3, 4, 5];
+// Accessing arry by index
+console.log(myArray[10]);
+// Correct the error
+console.log(myArray[4]);
---
+++ 22. Intentional Error/Intentional_Error.ts
@@ -0,0 +1,12 @@
+//Written by: Taha Ahmed
+//DATE: 16-2-24
+
+// Creating array
+let myArray :number[] = [1, 2, 3, 4, 5];
+
+// Accessing arry by index
+console.log(myArray[10]); 
+
+// Correct the error
+console.log(myArray[4]);
+
