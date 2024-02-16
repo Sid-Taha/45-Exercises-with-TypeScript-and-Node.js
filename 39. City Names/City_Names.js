5474a66f0b4108c619b69bccd40b92485b30c66d
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 23:45:50 +0500

    Add files via upload

---
+++ 39. City Names/City_Names.js
@@ -0,0 +1,11 @@
+"use strict";
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+// Function for city_country
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+// Result
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
---
+++ 39. City Names/City_Names.ts
@@ -0,0 +1,12 @@
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+
+// Function for city_country
+function city_country(city: string, country: string): string {
+  return `${city}, ${country}`;
+}
+
+// Result
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
