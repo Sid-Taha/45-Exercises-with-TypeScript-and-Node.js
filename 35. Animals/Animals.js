b6689ea2e9db8cf172477e0dcf886bd27047ac99
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 22:38:02 +0500

    Add files via upload

---
+++ 35. Animals/Animals.js
@@ -0,0 +1,11 @@
+"use strict";
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+// animals
+const animals = ["dog", "cat", "rabbit"];
+// statement about each animal
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+// Result
+console.log("Any of these animals would make a great pet!");
---
+++ 35. Animals/Animals.ts
@@ -0,0 +1,13 @@
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+
+// animals
+const animals: string[] = ["dog", "cat", "rabbit"];
+
+// statement about each animal
+for (let animal of animals) {
+  console.log(`A ${animal} would make a great pet.`);
+}
+
+// Result
+console.log("Any of these animals would make a great pet!");
