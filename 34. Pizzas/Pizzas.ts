83c19f690581604f2db7d77092637c0e391789b9
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 22:30:33 +0500

    Add files via upload

---
+++ 34. Pizzas/Pizzas.ts
@@ -0,0 +1,14 @@
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+
+//  names of the pizzas
+let pizzas: string[] = ["Pepperoni", "Cuban", "BBQ Chicken"];
+
+// Loop Use kiya he.
+for (let pizza of pizzas) {
+    
+    console.log(`I like ${pizza} pizza.`);
+}
+
+// Result
+console.log("I really love pizza!");
