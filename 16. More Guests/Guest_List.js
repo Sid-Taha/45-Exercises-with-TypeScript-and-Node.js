da08ea1fe387b7f83d87f4c166aef5230ca9a62b
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:55:08 +0500

    Add files via upload

---
+++ 16. More Guests/Guest_List.js
@@ -0,0 +1,19 @@
+"use strict";
+let Guest_List = ['Imran Khan', 'Nawaz Sharif', 'Asif Ali Zardari'];
+// for(let i=0; i<Guest_List.length; i++){
+//     console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+// }
+let absent_Guest = 'Imran Khan';
+let new_Guest = 'Kamran Tessori';
+Guest_List[0] = new_Guest;
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n');
+}
+console.log(`Mr. ${absent_Guest} is not coming to the party.`);
+console.log('Good News! We find Big Table So we are inviting 3 more guests.');
+Guest_List.unshift('Sir Zia Khan');
+Guest_List.splice(2, 0, 'Maryam Nawaz');
+Guest_List.push('Bilawal Bhutto Zardari');
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n');
+}
