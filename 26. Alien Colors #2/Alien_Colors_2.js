68215124822b2a42c9531c1f02082e7c2b394b2d
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 21:08:47 +0500

    Add files via upload

---
+++ 26. Alien Colors #2/Alien_Colors_2.js
@@ -0,0 +1,20 @@
+"use strict";
+//Written by: Taha Ahmed
+//DATE: 16-2-24
+// Color set kiya he.
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player just earned 5 ruppe for shooting the alien!");
+}
+else {
+    console.log("Player just earned 10 ruppe!");
+}
+// Color Change kar diya he. kam se kam 10 rupee to mil hi jaege.
+alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log("Player just earned 5 ruppe for shooting the alien!");
+}
+else {
+    // ye false kar k, 10 wali lays khaonga. :-)
+    console.log("Player just earned 10 ruppe!");
+}
