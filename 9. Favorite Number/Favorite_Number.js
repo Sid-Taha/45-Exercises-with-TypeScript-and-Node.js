bb84fad9b13fec45cefdb8a2e08200b453630f03
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:46:08 +0500

    Add files via upload

---
+++ 9. Favorite Number/Favorite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favorite_Number = 6;
+const message = 'Here is your favorite number ' + favorite_Number;
+console.log(message);
---
+++ 9. Favorite Number/Favorite_Number.ts
@@ -0,0 +1,7 @@
+
+
+const favorite_Number :number = 6 ;
+
+const message :string = 'Here is your favorite number ' + favorite_Number ;
+
+console.log(message) ;
