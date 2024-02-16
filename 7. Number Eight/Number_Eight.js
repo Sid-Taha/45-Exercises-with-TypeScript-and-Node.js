fd89f50576ee49dde78fe352d7d50d719124e4fd
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:42:57 +0500

    Add files via upload

---
+++ 7. Number Eight/Number_Eight.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(6 + 2); // addition
+console.log(10 - 2); // Subtraction
+console.log(4 * 2); // multiplication
+console.log(16 / 2); // divide
---
+++ 7. Number Eight/Number_Eight.ts
@@ -0,0 +1,9 @@
+
+
+console.log(6+2);  // addition
+
+console.log(10-2); // Subtraction
+
+console.log(4*2); // multiplication
+
+console.log(16/2); // divide
