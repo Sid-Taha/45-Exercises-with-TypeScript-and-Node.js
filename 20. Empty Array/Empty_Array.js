62107b2bb0bc0c2d6a3de32e86ef9c70544e10d4
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 21:00:13 +0500

    Add files via upload

---
+++ 20. Empty Array/Empty_Array.js
@@ -0,0 +1,8 @@
+"use strict";
+//Written by: Taha Ahmed
+//DATE: 16-2-24
+let countries = [];
+countries.push("Karachi");
+countries.push("Lahore");
+countries.push("Islamabad");
+console.log(countries);
---
+++ 20. Empty Array/Empty_Array.ts
@@ -0,0 +1,11 @@
+//Written by: Taha Ahmed
+//DATE: 16-2-24
+
+let countries :string[] = [];
+
+countries.push("Karachi");
+countries.push("Lahore");
+countries.push("Islamabad");
+
+
+console.log(countries);
