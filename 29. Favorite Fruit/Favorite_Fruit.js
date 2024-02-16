006e3f7da9a5e6db32bc58d6026ae0d0a88e3320
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 21:38:56 +0500

    Add files via upload

---
+++ 29. Favorite Fruit/Favorite_Fruit.js
@@ -0,0 +1,18 @@
+"use strict";
+//Written by: Taha Ahmed
+const favorite_fruits = ["banana", "apple", "strawberry"];
+if (favorite_fruits.includes("banana")) {
+    console.log("I like bananas!");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("I like apples!");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("I like strawberries!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("I like oranges!");
+}
+if (favorite_fruits.includes("kiwi")) {
+    console.log("I like kiwis!");
+}
