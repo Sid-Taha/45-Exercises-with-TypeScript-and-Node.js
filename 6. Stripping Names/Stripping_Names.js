4616c16b3eefd8f9922d47e74408cd3a8ccb0533
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:41:54 +0500

    Add files via upload

---
+++ 6. Stripping Names/Stripping_Names.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = '   \n\tShahid Afridi\t\n   ';
+console.log(personName);
+const without_Whitespace = personName.trim();
+console.log(without_Whitespace);
---
+++ 6. Stripping Names/Stripping_Names.ts
@@ -0,0 +1,9 @@
+
+
+const personName :string = '   \n\tShahid Afridi\t\n   ';
+
+console.log(personName);
+
+const without_Whitespace :string = personName.trim();
+
+console.log(without_Whitespace);
