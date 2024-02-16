5a92ebc63f4dd397c6147cdee712003169035dce
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:50:28 +0500

    Add files via upload

---
+++ 12. Greeting/greeting.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Talha', 'Asad', 'Mateen', 'Ejaz'];
+const message = 'Welcome to Typescript class: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12. Greeting/greeting.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Talha','Asad','Mateen','Ejaz'] ;
+
+const message :string = 'Welcome to Typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+    
+}
