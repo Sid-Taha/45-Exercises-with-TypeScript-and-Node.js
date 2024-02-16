06a6826e052a519e781d8a42729a0366c52d5803
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:49:21 +0500

    Add files via upload

---
+++ 11. Names/Name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Talha', 'Asad', 'Mateen', 'Ejaz'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11. Names/Name.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[] = ['Talha','Asad','Mateen','Ejaz'] ;
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+    
+}
