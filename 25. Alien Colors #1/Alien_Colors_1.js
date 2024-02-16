f6c3cb64659a97534cb5acc9711f9427b81a19a4
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 21:07:34 +0500

    Add files via upload

---
+++ 25. Alien Colors #1/Alien_Colors_1.js
@@ -0,0 +1,15 @@
+"use strict";
+//Written by: Taha Ahmed
+//DATE: 16-2-24
+// Alien's color define kiya he.
+let alien_color = 'green';
+//  Green color check kiya he.
+if (alien_color === 'green') {
+    console.log("Player just earned 5 Rupee!");
+}
+// Alien's color Change Kar diya he.
+alien_color = 'yellow';
+// ye code nahi chale ga kyn ke condition false he.
+if (alien_color === 'green') {
+    console.log("Player just earned 5 Ruppe!");
+}
---
+++ 25. Alien Colors #1/Alien_Colors_1.ts
@@ -0,0 +1,21 @@
+//Written by: Taha Ahmed
+//DATE: 16-2-24
+
+// Alien's color define kiya he.
+let alien_color :string = 'green';
+
+//  Green color check kiya he.
+if (alien_color === 'green') {
+    
+    console.log("Player just earned 5 Rupee!");
+}
+
+
+// Alien's color Change Kar diya he.
+ alien_color = 'yellow'; 
+
+// ye code nahi chale ga kyn ke condition false he.
+if (alien_color === 'green') {
+ 
+    console.log("Player just earned 5 Ruppe!");
+}
