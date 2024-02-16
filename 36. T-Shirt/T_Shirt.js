b9dd66466522f2c7e4591e23a4934e293d41d378
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 22:45:28 +0500

    Add files via upload

---
+++ 36. T-Shirt/T_Shirt.js
@@ -0,0 +1,7 @@
+"use strict";
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+function make_shirt(size, message) {
+    console.log(`Creating a ${size} shirt with the message: "${message}"`);
+}
+make_shirt('large', 'Hello, world!');
---
+++ 36. T-Shirt/T_Shirt.ts
@@ -0,0 +1,8 @@
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+
+function make_shirt(size: string, message: string): void {
+  console.log(`Creating a ${size} shirt with the message: "${message}"`);
+}
+
+make_shirt('large', 'Hello, world!');
