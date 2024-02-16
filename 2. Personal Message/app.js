90c995634aaa114ddbd34437b2b199af7d83939a
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:22:23 +0500

    Add files via upload

---
+++ 2. Personal Message/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// let personName :string = '';
+// personName = prompt('What is your name?') || '';
+// if(personName !== null && personName !== ''){
+//     alert(`Hello ${personName}, would you like to learn some Python today?`)
+// }
+// else{
+//     alert('You have to fill your name !')
+// }
+let personName = 'Taha';
+prompt('what is you name?');
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
---
+++ 2. Personal Message/app.ts
@@ -0,0 +1,15 @@
+// let personName :string = '';
+
+// personName = prompt('What is your name?') || '';
+
+// if(personName !== null && personName !== ''){
+//     alert(`Hello ${personName}, would you like to learn some Python today?`)
+// }
+// else{
+//     alert('You have to fill your name !')
+// }
+
+
+let personName :string = 'Taha';
+
+console.log(`Hello ${personName}, would you like to learn some Python today?`)
