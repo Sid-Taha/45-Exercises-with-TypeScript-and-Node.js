7651f72eced3e93a8698a6df55c72c28acd5ac9b
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 22:00:40 +0500

    Add files via upload

---
+++ 31. No Users/No_Users.ts
@@ -0,0 +1,20 @@
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+
+// array for usernames
+let usernames: string[] = ['admin', 'Junaid', 'Zeeshan', 'Kamran', 'Daniyal'];
+
+// Checking User Availiblity 
+if (usernames.length === 0) {
+  console.log("We need to find some users!");
+} else {
+  // Removimg all usernames from the array
+  usernames = [];
+  console.log("All usernames have been removed. " + usernames.length);
+}
+
+
+
+
+
+
