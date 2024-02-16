a7233d7e347f19e23de07c4e73db4026361b13a8
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 20:28:39 +0500

    Add files via upload

---
+++ 5. Famous Quote 2/Famous_Quote_2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'Albert Einstein';
+let message = '“A person who never made a mistake never tried anything new.”';
+console.log(`${famous_person} once said, ${message}`);
---
+++ 5. Famous Quote 2/Famous_Quote_2.ts
@@ -0,0 +1,5 @@
+let famous_person :string = 'Albert Einstein';
+
+let message :string = '“A person who never made a mistake never tried anything new.”';
+
+console.log(`${famous_person} once said, ${message}`);
