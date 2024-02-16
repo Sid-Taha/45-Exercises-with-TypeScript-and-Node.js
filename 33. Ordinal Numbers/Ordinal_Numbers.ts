1bcdb22b9350341d4fc83e33eec83ad6eadad77f
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-16 22:22:43 +0500

    Add files via upload

---
+++ 33. Ordinal Numbers/Ordinal_Numbers.ts
@@ -0,0 +1,25 @@
+//Written by: Taha Ahmed
+// Roll No. : 00298983
+
+// numbers from 1 to 9.
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// using a for loop.
+for (let i = 0; i < numbers.length; i++) {
+    
+    const number = numbers[i];
+    let ordinalEnding: string;
+
+    if (number === 1) {
+        ordinalEnding = "st";
+    } else if (number === 2) {
+        ordinalEnding = "nd";
+    } else if (number === 3) {
+        ordinalEnding = "rd";
+    } else {
+        ordinalEnding = "th";
+    }
+
+    // Printing Answaere
+    console.log(`${number}${ordinalEnding}`);
+}
