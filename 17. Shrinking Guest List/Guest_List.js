eb696ac979105ab752d81b221d5ed7474b83d508
Sid-Taha <116095101+Sid-Taha@users.noreply.github.com>
2024-02-29 04:01:46 +0500

    Update Guest_List.js

--- 17. Shrinking Guest List/Guest_List.js
+++ 17. Shrinking Guest List/Guest_List.js
@@ -9,8 +9,8 @@ Guest_List[0] = new_Guest;
 // for(let i=0; i<Guest_List.length; i++){
 //     console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
 // }
-console.log(`Mr. ${absent_Guest} is not coming to the party.`);
-console.log('Good News! We find Big Table So we are inviting 3 more guests.');
+// console.log(`Mr. ${absent_Guest} is not coming to the party.`);
+// console.log('Good News! We find Big Table So we are inviting 3 more guests.');
 Guest_List.unshift('Sir Zia Khan');
 Guest_List.splice(2, 0, 'Maryam Nawaz');
 Guest_List.push('Bilawal Bhutto Zardari');
