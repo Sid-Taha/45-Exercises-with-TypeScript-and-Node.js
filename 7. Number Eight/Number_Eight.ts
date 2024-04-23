554eb54444868b4ff901e9d233166c90a05b2339
Taha Ahmed Siddiqui <116095101+Sid-Taha@users.noreply.github.com>
2024-04-23 23:18:27 +0000

    commect added

--- 7. Number Eight/Number_Eight.ts
+++ 7. Number Eight/Number_Eight.ts
@@ -1,8 +1,8 @@
 
 
-console.log(6+2);  // addition
+console.log(6+2);  // addition method by me
 
-console.log(10-2); // Subtraction
+console.log(10-2); // Subtraction method
 
 console.log(4*2); // multiplication
 
